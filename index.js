c1d4b236cddb6731ce6dbb6688e1f5d3ea45238b
Curtis Maloney <curtis@tinbrain.net>
2018-06-26 10:51:53 +1000

    Add esdoc comments

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 
+/**
+ * @desc CSV generator
+ * @param {Array<String>} fields - List of field keys
+ * @param {Object} [options]
+ * @param {Array<String>} [options.headers] - Field headings
+ * @param {String} [options.SEP] - Field separator (Default: ',')
+ * @param {String} [options.QUOTE] - Field quote character (Default: '"')
+ * @param {String} [options.LINEBREAK] - Record separator (Default: '\n')
+ * @param {String} [options.ESCQUOTE] - Character to replace QUOTE with when in value (Default: '""')
+ */
 export class CSVWriter {
-	constructor (fields, headers, options) {
+	constructor (fields, options) {
 		options = Object.assign({SEP: ',', QUOTE: '"', LINEBREAK: '\n'}, options);
-		let {SEP, QUOTE, LINEBREAK, ESCQUOTE} = options;
+		let {SEP, QUOTE, LINEBREAK, ESCQUOTE, headers} = options;
 
 		let QUOTEre = new RegExp(QUOTE, 'g')
 		this.escape_field = (value) => {
@@ -18,14 +28,28 @@ export class CSVWriter {
 		this.headers = headers
 	}
 
+	/**
+	 * @desc Generate a row of values
+	 * @param {Array<any>} values
+	 * @returns {String}
+	 */
 	write (values) {
 		return values.map(this.escape_field).join(this.SEP) + this.LINEBREAK;
 	}
 
+	/**
+	 * @desc Generate a row, pulling values from an object
+	 * @params {Object} obj
+	 * @returns {String}
+	 */
 	write_map (obj) {
 		return this.write(this.fields.map(f => obj[f]))
 	}
 
+	/**
+	 * @desc Generate a row from the field headers
+	 * @returns {String}
+	 */
 	write_headers () {
 		return this.write(this.headers)
 	}
